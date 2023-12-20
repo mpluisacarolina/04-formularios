82d6db3185f8ed803b1aa1bb9f9a8fa60cfb5e42
mpluisacarolina <mpluisacarolina@gmail.com>
2023-12-20 15:37:07 -0400

    Spread Syntax (Sintaxis Expandida), Modificaciones en el envío de formularios

--- src/Components/FormularioComponent.jsx
+++ src/Components/FormularioComponent.jsx
@@ -2,9 +2,7 @@ import React, { useState } from 'react'
 
 export const FormularioComponent = () => {
 
-    const [usuario, setUsuario] = useState({
-
-    });
+    const [usuario, setUsuario] = useState({});
 
     const conseguirDatosFormulario = e => {
         e.preventDefault()
@@ -13,38 +11,60 @@ export const FormularioComponent = () => {
             nombre: datos.nombre.value,
             apellido: datos.apellido.value,
             genero: datos.genero.value,
-            bio: datos.bio.value
+            bio: datos.bio.value,
+            enviar: datos.enviar.value
         }
         console.log(usuario);
         setUsuario(usuario);
+    }
+
+    const cambiarDatos = e => {
+        let nameDelInput = e.target.name;
+
+        setUsuario(estadoPrevio => ({
+            ...estadoPrevio,
+            [nameDelInput]: e.target.value
+        })
+        );
 
     }
+
     return (
         <div>
             <h1>Formularios con React</h1>
-            { usuario.bio && usuario.nombre && usuario.apellido && usuario.bio.length >= 1 &&
-            (
-            <div className = "info_usuario label label-pink">
-                {usuario.nombre} {usuario.apellido} es {usuario.genero} y su biografía es: {usuario.bio}
-            </div>
-            )}
+            { usuario.enviar &&
+                (
+                    <div className="info_usuario label label-pink">
+                        {usuario.nombre} {usuario.apellido} es {usuario.genero} y su biografía es: {usuario.bio}
+                    </div>
+                )
+            }
 
+            <form onSubmit={conseguirDatosFormulario}>
+                <input type="text"
+                    placeholder='Nombre'
+                    name="nombre"
+                    onChange = {cambiarDatos}
+                />
 
-            <form onSubmit = {conseguirDatosFormulario}>
-                <input type = "text" 
-                    placeholder = 'Nombre' 
-                    name = "nombre"
+                <input type="text"
+                    placeholder='Apellido'
+                    name="apellido"
+                    onChange = {cambiarDatos}
                 />
-                <input type="text" 
-                    placeholder='Apellido' 
-                    name = "apellido"
-                />
-                <select name = "genero">
+
+                <select name="genero" onChange = {cambiarDatos}>
                     <option value="hombre"> Hombre </option>
                     <option value="mujer"> Mujer </option>
                 </select>
-                <textarea placeholder='Biografía' name = "bio"></textarea>
-                <input type="submit" value="Enviar" />
+
+                <textarea placeholder='Biografía'
+                    name="bio"
+                    onChange = {cambiarDatos}>
+                </textarea>
+
+                <input type="submit" value="Enviar" name="enviar" />
+
             </form>
         </div>
     )
