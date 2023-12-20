2e46853da726814cb4f87a5e580cd62eea88308e
mpluisacarolina <mpluisacarolina@gmail.com>
2023-12-20 10:30:37 -0400

    Cambios en App.css

--- src/Components/FormularioComponent.jsx
+++ src/Components/FormularioComponent.jsx
@@ -1,19 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const FormularioComponent = () => {
-  return (
-    <div>
-        <h1>Formularios con React</h1>
-        <form>
-            <input type="text" placeholder='Nombre' />
-            <input type="text" placeholder='Apellido' />
-            <select>
-                <option value = "hombre"> Hombre </option>
-                <option value = "mujer"> Mujer </option>
-            </select>
-            <textarea placeholder = 'Biografía'></textarea>
-            <input type = "submit" value = "Enviar" />
-        </form>
-    </div>
-  )
+
+    const [usuario, setUsuario] = useState({
+
+    });
+
+    const conseguirDatosFormulario = e => {
+        e.preventDefault()
+        let datos = e.target;
+        let usuario = {
+            nombre: datos.nombre.value,
+            apellido: datos.apellido.value,
+            genero: datos.genero.value,
+            bio: datos.bio.value
+        }
+        console.log(usuario);
+        setUsuario(usuario);
+
+    }
+    return (
+        <div>
+            <h1>Formularios con React</h1>
+
+            <div className = "info_usuario label label-gray">
+                {usuario.nombre} {usuario.apellido} es un {usuario.genero} y su biografía es: {usuario.bio}
+            </div>
+
+
+            <form onSubmit = {conseguirDatosFormulario}>
+                <input type = "text" 
+                    placeholder = 'Nombre' 
+                    name = "nombre"
+                />
+                <input type="text" 
+                    placeholder='Apellido' 
+                    name = "apellido"
+                />
+                <select name = "genero">
+                    <option value="hombre"> Hombre </option>
+                    <option value="mujer"> Mujer </option>
+                </select>
+                <textarea placeholder='Biografía' name = "bio"></textarea>
+                <input type="submit" value="Enviar" />
+            </form>
+        </div>
+    )
 }
